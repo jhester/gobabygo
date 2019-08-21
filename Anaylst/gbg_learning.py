b5634ec3dce4ee03eff9397be6c675d94e985dcf
Tsong <yucongchen2017@u.northwestern.edu>
2019-08-21 09:26:45 -0500

    Utilize magneto for direction

--- Anaylst/gbg_learning.py
+++ Anaylst/gbg_learning.py
@@ -6,14 +6,15 @@ Created on Mon Aug 12 20:24:35 2019
 """
 import os
 import numpy as np
-from math import atan2,pi, degrees,radians
+from numpy import arctan, arctan2, sin, cos
+from math import atan, atan2,pi, degrees,radians
 import pandas as pd
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 
 #1000 mg = 9.80665 m/s2
 
-df=pd.read_csv('Data/SensorTile_Log_N4.csv')
+df=pd.read_csv('Data/SensorTile_Log_N2.csv')
 
 def line_graph(df):
     fig, axes = plt.subplots(nrows=3, ncols=1)
@@ -64,10 +65,17 @@ def scatter(df):
 #normalize v that |v|=1
 def normalize(v):
     m=np.sqrt(np.sum(v**2))
-    v=v/m
-    return v
+    if m==0:
+        print(v)
+        return v
+    return v/m
     
-
+#X: left, Y:Front Z:Up
+#Azimuth=arctan(Hx/Hy)
+#pitch=arctan(Ax/sqrt(Ay**2+Az**2))
+#roll=arctan(Ay/sqrt(Ax**2+Az**2))
+#Hx=My*cos(roll)-Mz*sin(roll)
+#Hy=Mx*cos(pitch)+My*sin(pitch)*sin(roll)+Mz*sin(pitch)*cos(roll)
 
 standard_g=9.80665 #1g=9.80665 m/s2
 delta_t=0.1 #second
@@ -75,45 +83,63 @@ cos45=1/np.sqrt(2)
 acc_threshold=100
 R=np.array([-cos45, 0, cos45])
 direction=[0]
+acc_zero_error=[941, 223, -215]
+gyro_zero_error=[-70, 1050, 1050]
+mag_zero_error=[52, -774, 187]
 
 prev_v=np.zeros(2)
 prev_a=np.zeros(2)
 prev_pos=np.zeros(2)
 positions=[]
 distance=[0]
+direction=[]
 
 for index, row in df.head(1000).iterrows():
     #detect static, if values are the same in 10 frames(1 sec), update zero_error with avg values
     accel=np.array([row['AccX [mg]'],row['AccY [mg]'],row['AccZ [mg]']])#unit:mg
     gyro=np.array([row['GyroX [mdps]'],row['GyroY [mdps]'],row['GyroZ [mdps]']])#unit:mdps
+    mag=np.array([row['MagX [mgauss]'],row['MagY [mgauss]'],row['MagZ [mgauss]']])
     static=True
     acc_avg=np.array([row['AccX [mg]'],row['AccY [mg]'],row['AccZ [mg]']])
     gyro_avg=np.array([row['GyroX [mdps]'],row['GyroY [mdps]'],row['GyroZ [mdps]']])
+    mag_avg=np.array([row['MagX [mgauss]'],row['MagY [mgauss]'],row['MagZ [mgauss]']])
     for i in range(index+1, index+10):
         tmp=np.array([df.iloc[i]['AccX [mg]'],df.iloc[i]['AccY [mg]'],df.iloc[i]['AccZ [mg]']])
         diff=np.sum((tmp-accel)**2)
         acc_avg+=tmp
         gyro_avg+=np.array([df.iloc[i]['GyroX [mdps]'],df.iloc[i]['GyroY [mdps]'],df.iloc[i]['GyroZ [mdps]']])
+        mag_avg+=np.array([df.iloc[i]['MagX [mgauss]'],df.iloc[i]['MagY [mgauss]'],df.iloc[i]['MagZ [mgauss]']])
         if diff>acc_threshold:
             static=False
             break;
     if static is True:
         acc_zero_error=acc_avg/10
         gyro_zero_error=gyro_avg/10
+        mag_zero_error=mag_avg/10
         #print('acc zero error',acc_zero_error)
         #print('gyro zero error',gyro_zero_error)
+        #print('mag zero error',mag_zero_error)
         
     #adjust acc and gyro values
-    Racc=(accel.astype(float)-acc_zero_error)/1000.0#unit:g
-    Racc=normalize(Racc)
+    acc=(accel.astype(float)-acc_zero_error)/1000.0#unit:g
+    acc=normalize(acc)
     gyro=(gyro.astype(float)-gyro_zero_error)/1000.0#unit:dps
+    mag=(mag.astype(float)-mag_zero_error)
+    
+    pitch=arctan2(acc[0],np.sqrt(acc[1]**2+acc[2]**2))
+    roll=arctan2(acc[1],np.sqrt(acc[0]**2+acc[2]**2))
+    Azimuth=arctan2((mag[1]*cos(roll)-mag[2]*sin(roll)),(mag[0]*cos(pitch)+mag[1]*sin(pitch)*sin(roll)+mag[2]*sin(pitch)*cos(roll)))
+    
+    
     angle_speed=np.sqrt(gyro[0]**2+gyro[2]**2)*np.sign(gyro[0])
-    direction.append((direction[-1]+angle_speed*delta_t)%360)
+    #direction.append((direction[-1]+angle_speed*delta_t)%360)
+    direction.append(Azimuth)
     
     #F+G=[x,y,z]=[x'cos45+z'cos45, y, -x'cos45+z'cos45]
     a=np.array([accel[0]*cos45+accel[2]*cos45, accel[1]]) *standard_g
     #numeriacal integration
     v= prev_v + (a+prev_a)/2*delta_t
+    #rotate v with direction
     #x'=x*cos(theta)+y*cos(theto)
     #y'=-x*sin(theta)+y*cos(theta)
     new_x = v[0] * np.cos(np.deg2rad(direction[-1])) + v[1] * np.sin(np.deg2rad(direction[-1]))
@@ -135,7 +161,7 @@ plt.ylabel("Y")
 plt.title("track")
 plt.show()
 
-plt.plot(np.arange(0,100.1,0.1), direction)
+plt.plot(np.arange(0,100,0.1), direction)
 plt.title("Turns")
 plt.xlabel("time [s]")
 plt.ylabel("Angle [degree]")
